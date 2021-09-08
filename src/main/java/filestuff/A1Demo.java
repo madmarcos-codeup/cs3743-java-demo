42d84248968d533f95f6b10baaddbe0a9dab3bb3
docrob <mark.robinson@utsa.edu>
2021-09-08 12:58:43 -0500

    post class sept 8

---
+++ src/main/java/filestuff/A1Demo.java
@@ -0,0 +1,17 @@
+package filestuff;
+
+public class A1Demo {
+//    public static int writeRec(HashFile hashFile, int rbn, Employee employee) {
+//        int rba = rbn * hashFile.getHashHeader().getRecSize();
+//        try {
+//            hashFile.getFile().seek(rba);
+//            char [] chars = employee.toFileChars();
+//            for(int i = 0; i < chars.length; i++)
+//                hashFile.getFile().writeChar(chars[i]);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//            return ReturnCodes.RC_LOC_NOT_FOUND;
+//        }
+//        return ReturnCodes.RC_OK;
+//    }
+}
